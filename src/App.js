1b048547f3344763562ae9cef63c5343dc0d7763
HwangInSeong1 <dlstjd2029@naver.com>
2021-07-22 23:29:51 +0900

    App.js 수정
    11111

--- src/App.js
+++ src/App.js
@@ -15,7 +15,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Soon Later
+          Soon Later11111
         </a>
       </header>
     </div>
